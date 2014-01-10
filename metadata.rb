cf6e1ca3a3a94a16e200a3fbfcc65445244f48cf
Robert J. Berger <rberger@ibd.com>
2014-01-09 17:55:50 -0800

    First pass

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,7 @@ description      'Installs/Configures rm_ohai'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+%w(ohai client-config).each do |dp|
+  depends dp
+end
+
--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,12 @@
 # 
 # All rights reserved - Do Not Redistribute
 #
+
+# Tells the ohai cookbook to tell the chef-client cookbook to set the Ohai::Config[:plugin_path] in the node's client.rb
+default.node['ohai']['plugin_path'] = "/etc/chef/ohai_plugins"
+
+# Tells the ohai cookbook to use the plugins in this cookbook as the source for the plugins to install
+default.node['ohai']['plugins']['rm_ohai'] = "plugins"
+
+include_recipe 'ohai::defualt'
+include_recipe 'chef-client::config'
